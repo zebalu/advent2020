70826e78ff1bdd39ae968a909c68e8279c511465
Balazs Zaicsek <zebalu@gmail.com>
2020-12-06 21:17:21 +0100

    refactor

--- day_03/src/main/kotlin/io/github/zebalu/advent2020/TobboganMap.kt
+++ day_03/src/main/kotlin/io/github/zebalu/advent2020/TobboganMap.kt
@@ -3,7 +3,7 @@ package io.github.zebalu.advent2020
 import java.util.HashMap
 
 class TobboganMap {
-	private val map: MutableMap<Pair<Int, Int>, String> = HashMap();
+	private val map: Map<Pair<Int, Int>, String>;
 	private val width: Int;
 	private val height: Int;
 
@@ -11,14 +11,16 @@ class TobboganMap {
 		width = lines[0].length;
 		height = lines.size;
 		var y = 0
+		val collectorMap = HashMap<Pair<Int, Int>, String>();
 		while (y < height) {
 			var x = 0
 			while (x < width) {
-				map.put(Pair(x, y), "" + lines[y][x])
+				collectorMap.put(Pair(x, y), "" + lines[y][x])
 				++x
 			}
 			++y
 		}
+		map = collectorMap
 	}
 
 	fun countTrees(from: Pair<Int, Int>, slope: Pair<Int, Int>): Int {
--- day_03/src/main/kotlin/io/github/zebalu/advent2020/TobboganTreeHiterBySlope.kt
+++ day_03/src/main/kotlin/io/github/zebalu/advent2020/TobboganTreeHiterBySlope.kt
@@ -1,18 +1,15 @@
 package io.github.zebalu.advent2020
 
 object TobboganTreeHiterBySlope {
-	fun countByBaseSlope(input: List<String>):Int {
+	fun countByBaseSlope(input: List<String>): Int {
 		val map = TobboganMap(input)
-		return map.countTrees(Pair(0,0), Pair(3,1))
+		return map.countTrees(Pair(0, 0), Pair(3, 1))
 	}
-	
-	fun countByAllSlope(input: List<String>):Int {
+
+	fun countByAllSlope(input: List<String>): Int {
+		val slopes = listOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
 		val map = TobboganMap(input)
-		val start = Pair(0,0)
-		return map.countTrees(start, Pair(1,1)) *
-				map.countTrees(start, Pair(3,1)) *
-				map.countTrees(start, Pair(5,1)) *
-				map.countTrees(start, Pair(7,1)) *
-				map.countTrees(start, Pair(1,2))
+		val start = Pair(0, 0)
+		return slopes.map { slope -> map.countTrees(start, slope) }.fold(1, { acc, prod -> acc * prod })
 	}
 }
