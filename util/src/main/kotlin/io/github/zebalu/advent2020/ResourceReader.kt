7918a2212dbd5355a7798f3f830c0abdeb1cff92
Balazs Zaicsek <zebalu@gmail.com>
2020-12-06 10:18:59 +0100

    make resource reading more functional

--- util/src/main/kotlin/io/github/zebalu/advent2020/ResourceReader.kt
+++ util/src/main/kotlin/io/github/zebalu/advent2020/ResourceReader.kt
@@ -2,23 +2,29 @@ package io.github.zebalu.advent2020
 
 object ResourceReader {
 	fun lines(resName: String): List<String> {
-		return ResourceReader::class.java.getResource(resName).readText().lines()
+		return getText(resName).lines()
 	}
-	
+
 	fun lineGroups(resName: String): List<List<String>> {
-		val result = ArrayList<List<String>>()
-		val group = ArrayList<String>()
-		lines(resName).forEach { line ->
-			if(line.isEmpty()) {
-				result.add(ArrayList(group))
-				group.clear()
-			} else {
-				group.add(line)
-			}
-		}
-		if(!group.isEmpty()) {
-			result.add(group)
-		}
-		return result
+		return splitToLineGroupsByEmptyLines(getText(resName)).map { g -> g.lines() }
+	}
+
+	private fun getText(resName: String): String {
+		return ResourceReader::class.java.getResource(resName).readText()
+	}
+
+	private fun getSeparator(text: String): String {
+		return if (text.contains("\r\n")) "\r\n"
+		else if (text.contains("\n\r")) "\n\r"
+		else if (text.contains("\n")) "\n"
+		else throw IllegalStateException("Separator can not be determined")
+	}
+
+	private fun splitToLineGroupsByEmptyLines(text: String): List<String> {
+		return splitToLineGroupsByEmptyLines(text, getSeparator(text))
+	}
+
+	private fun splitToLineGroupsByEmptyLines(text: String, separator: String): List<String> {
+		return text.split(separator + separator)
 	}
 }
