70a895a985acdc63e0f2b7dee96de75f81b44432
Balazs Zaicsek <zebalu@gmail.com>
2020-12-12 08:19:43 +0100

    remove Java specific class

--- day_03/src/main/kotlin/io/github/zebalu/advent2020/TobboganMap.kt
+++ day_03/src/main/kotlin/io/github/zebalu/advent2020/TobboganMap.kt
@@ -1,9 +1,7 @@
 package io.github.zebalu.advent2020
 
-import java.util.HashMap
-
 class TobboganMap {
-	private val map: Map<Pair<Int, Int>, String>;
+	private val map: Map<Pair<Int, Int>, Char>;
 	private val width: Int;
 	private val height: Int;
 
@@ -11,11 +9,11 @@ class TobboganMap {
 		width = lines[0].length;
 		height = lines.size;
 		var y = 0
-		val collectorMap = HashMap<Pair<Int, Int>, String>();
+		val collectorMap = mutableMapOf<Pair<Int, Int>, Char>();
 		while (y < height) {
 			var x = 0
 			while (x < width) {
-				collectorMap.put(Pair(x, y), "" + lines[y][x])
+				collectorMap.put(Pair(x, y), lines[y][x])
 				++x
 			}
 			++y
@@ -26,12 +24,12 @@ class TobboganMap {
 	fun countTrees(from: Pair<Int, Int>, slope: Pair<Int, Int>): Int {
 		var pos = from
 		var count = 0
-		if (map.get(pos).equals("#")) {
+		if (map.get(pos) == '#') {
 			++count
 		}
 		while (pos.second < height) {
 			pos = Pair((pos.first + slope.first) % width, pos.second + slope.second)
-			if (map.get(pos).equals("#")) {
+			if (map.get(pos) == '#') {
 				++count
 			}
 		}
